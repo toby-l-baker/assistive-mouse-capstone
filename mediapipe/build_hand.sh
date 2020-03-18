aa5890c1a56b5154f8cc4a088e32aeb51104e177
Ayusman Saha <AyusmanSaha@gmail.com>
2020-03-18 12:33:21 -0700

    reverting build to bazel instead of bazel-1.2.1

--- mediapipe/build_hand.sh
+++ mediapipe/build_hand.sh
@@ -7,5 +7,5 @@
 #
 
 
-bazel-1.2.1 build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
+bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
     mediapipe/examples/desktop/hand_tracking:hand_tracking_cpu
